2773449141b8a980e6a004328b42d6012f103e30
Michal Debski <michal.d9777@gmail.com>
2025-09-01 19:24:30 +0200

    next step

---
+++ FoodOrderingApp/src/components/homepage-owner/home-owner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home-owner.component.html',
+  styleUrl: './home-owner.component.css'
+})
+export class HomeOwnerComponent {
+  heroImage: string = '../../assets/images/background.jpg';
+
+}
---
+++ FoodOrderingApp/src/services/auth.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+import {LoginRequest} from '../models/login-request';
+import {LoginResponse} from '../models/login-response';
+
+
+const API_URL = "http://localhost:8222/api/v1/auth/login"
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {
+  }
+
+  login(request: LoginRequest): Observable<LoginResponse> {
+    tap({
+      next: (res: any) => {
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('role', res.role);
+      },
+      error: (err) => console.error('Login error', err)
+    })
+    return this.http.post<LoginResponse>(API_URL, request);
+  }
+}
